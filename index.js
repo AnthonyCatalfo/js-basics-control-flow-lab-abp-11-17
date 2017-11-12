67bb5462aa4bfc831f9bc1ae6c6d5ca9b925be1d
AnthonyCatalfo <anthonycatalfo@gmail.com>
2017-11-12 15:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+function scuberGreetingForFeet(rideLength){
+  switch(rideLength) {
+    case <=400:
+        return 'This one is on me!'
+        break;
+    case <2000:
+        return 'I will gladly take your thirty bucks.'
+        break;
+    case <2500:
+      return 'No can do.'
+      break;
+    default:
+        return "?"
+      }
+}
+//result = x > y ? "good job" : 20;
+function ternaryCheckCity(city) {
+  return city==='NYC' ? 'Ok, sounds good.': 'No go.';
+}
